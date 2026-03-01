f6fb0b9b8e97cb9f4451b391d20f00080920a6a9
Hélio <heliocg@zoho.com>
2026-03-01 20:40:27 -0300

    Ajustes

--- setup.sh
+++ setup.sh
@@ -1,34 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 # Verificar se é root
-if [ "$EUID" -ne 0 ]; then
-    echo "❌ Este script precisa ser executado como root."
-    exit 1
+if [ "x$(id -u)" != 'x0' ]; then
+	echo 'Error: this script can only be executed by root'
+	exit 1
 fi
 
-# Verificar se é Debian
-if [ ! -f /etc/os-release ]; then
-    echo "❌ Não foi possível identificar o sistema."
-    exit 1
+# Detect OS
+if [ -e "/etc/os-release" ] && [ ! -e "/etc/redhat-release" ]; then
+	type=$(grep "^ID=" /etc/os-release | cut -f 2 -d '=')
+    if [ "$type" = "debian" ]; then
+		release=$(cat /etc/debian_version | grep -o "[0-9]\{1,2\}" | head -n1)
+		VERSION='debian'
+	else
+		type="NoSupport"
+	fi
+else
+	type="NoSupport"
 fi
 
-. /etc/os-release
+no_support_message() {
+	echo "****************************************************"
+	echo "Your operating system (OS) is not supported by"
+	echo "PainelStream. Officially supported releases:"
+	echo "****************************************************"
+	echo "  Debian 12, 13"
+	echo ""
+	exit 1
+}
 
-if [ "$ID" != "debian" ]; then
-    echo "❌ Este script só funciona em Debian."
-    exit 1
+if [ "$type" = "NoSupport" ]; then
+	no_support_message
 fi
 
-# Verificar versão Debian 13+
-if [ "$VERSION_ID" -lt 13 ]; then
-    echo "❌ É necessário Debian 13 ou superior."
-    exit 1
-fi
-
+# Atualiza e instala pacotes
 apt update && apt upgrade -y
 apt install git rsync -y
 
-cd /usr/local
-git clone git@github.com:helio-cg/painelstream-server.git painelstream
+cd /usr/local && git clone git@github.com:helio-cg/painelstream-server.git painelstream
 
 cd /usr/local/painelstream
 sh ./install-icecast.sh
