0fe114d5a821fc9f173de0e02d91052427829de0
Hélio <helio@elicast.app>
2026-04-21 17:45:42 -0300

    Ajustes

--- setup/nginx-install.sh
+++ setup/nginx-install.sh
@@ -20,10 +20,6 @@ server {
 
     location ^~ /app/ {
         proxy_pass http://127.0.0.1:3000/;
-
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }
 
     location / {
